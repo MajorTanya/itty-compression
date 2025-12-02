64540b18a814b9eab26d6435d2cb3242134a323e
MajorTanya <github@majortanya.eu>
2025-12-02 01:46:30 +0100

    Fix deprecated boolean for allowTemplateLiterals
    "always" is the equivalent setting for literal true.
    
    see: https://eslint.style/rules/quotes#options

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -41,7 +41,7 @@ export default [
         'error',
         'single',
         {
-          allowTemplateLiterals: true,
+          allowTemplateLiterals: 'always',
           avoidEscape: true,
         },
       ],
